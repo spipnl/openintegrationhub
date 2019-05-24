a417f439b9fd0595c8073cf2300455944a05c2b8
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-05-24 14:31:39 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -178,6 +178,34 @@ describe('User Routes', () => {
 		
 		done();   		
 	});
+	
+	test('--- TIMEOUT ---', async (done) => {
+        const jsonPayload = {
+        	username,
+        	password
+		};
+        const Login = {
+        	method: 'POST',
+        	uri: `http://iam.openintegrationhub.com/login`,
+        	json: true,
+        	body: jsonPayload
+        };
+		const response = await request(Login);
+
+		const getToken = async res => {
+			try {
+				token = await Promise.resolve(res.body.token);
+			}
+			catch (error) {
+				console.log(error);
+			}
+			return token; 
+		};	
+
+		tokenAdmin = await getToken(response); 
+		expect(response.statusCode).toEqual(200);	
+    	done();
+    });	
 
 	test('--- STOP FLOW BY ID ---', async (done) => { 
 		var status = false;
