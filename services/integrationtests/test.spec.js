8a2cd8b236a91c65411cd2d6bc2d8555be000b60
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-07-23 11:17:57 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -16,45 +16,18 @@ let componentID = null;
 
 describe('User Routes', () => {
    jest.setTimeout(15000);
-   //tokenAdmin = importToken.token;
-  
-   test('--- IAM 1. LOGIN & TOKEN ---', async (done) => {
-	console.log("imported: " + importToken.token);
-        const jsonPayload = {
-          	username,
-        	password
-		    };
-        const Login = {
-        	method: 'POST',
-        	uri: `http://iam.openintegrationhub.com/login`,
-        	json: true,
-        	body: jsonPayload
-        };
-	      const response = await request(Login);
-
-	      const getToken = async res => {
-		    try {
-			      token = await Promise.resolve(res.body.token);
-		        }
-		        catch (error) {
-			      console.log(error);
-		        }
-		        return token; 
-	      };	
-
-	      tokenAdmin = await getToken(response);
-	      expect(response.statusCode).toEqual(200);
-    done();
-    });
 	
-   test('--- FLOW REPO 2. GET All FLOWS ---', async (done) => { 
-      const getAllFlows = {
+   test('--- FLOW REPO 2. GET All FLOWS ---', async (done) => {
+	   
+	tokenAdmin = importToken.token;
+	   
+	const getAllFlows = {
             	method: 'GET',
             	uri: `http://flow-repository.openintegrationhub.com/flows`,
             	headers: {
                 	"Authorization" : " Bearer " + tokenAdmin, 
             	}
-      };
+      	};
      const response = await request(getAllFlows);
      expect(response.statusCode).toEqual(200);
      done();
