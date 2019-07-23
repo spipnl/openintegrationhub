2d503688febe8b58f11a4de1796b48e8100d3996
DennisCES <38039738+DennisCES@users.noreply.github.com>
2019-07-23 10:57:05 +0200

    Update test.spec.js

--- services/integrationtests/test.spec.js
+++ services/integrationtests/test.spec.js
@@ -16,9 +16,36 @@ let componentID = null;
 
 describe('User Routes', () => {
    jest.setTimeout(15000);
-   tokenAdmin = importToken.token;
-	//console.log("imported: " + importToken.token);
+   //tokenAdmin = importToken.token;
+   console.log("imported: " + importToken.token);
+   test('--- IAM 1. LOGIN & TOKEN ---', async (done) => {
+        const jsonPayload = {
+          	username,
+        	password
+		    };
+        const Login = {
+        	method: 'POST',
+        	uri: `http://iam.openintegrationhub.com/login`,
+        	json: true,
+        	body: jsonPayload
+        };
+	      const response = await request(Login);
 
+	      const getToken = async res => {
+		    try {
+			      token = await Promise.resolve(res.body.token);
+		        }
+		        catch (error) {
+			      console.log(error);
+		        }
+		        return token; 
+	      };	
+
+	      tokenAdmin = await getToken(response);
+	      expect(response.statusCode).toEqual(200);
+    done();
+    });
+	
    test('--- FLOW REPO 2. GET All FLOWS ---', async (done) => { 
       const getAllFlows = {
             	method: 'GET',
